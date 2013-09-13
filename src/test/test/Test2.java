0633b776c6e5aab539105b2bbafd0d5ec94aff03
viktor <sh_maker@mail.ru>
2013-09-13 14:22:10 +0300

    comiy 2

---
+++ src/test/test/Test2.java
@@ -0,0 +1,23 @@
+package test.test;
+
+public class Test2 {
+
+	private int i, i2;
+
+	public int getI() {
+		return i;
+	}
+
+	public int getI2() {
+		return i2;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public void setI2(int i2) {
+		this.i2 = i2;
+	}
+
+}
